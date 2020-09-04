8518eaca8734db25b0bcd38ece5dd22da4b44a8f
teknofage <teknofage@gmail.com>
2020-09-03 20:07:47 -0700

    factorial_iterative complete

--- recursion-and-search/recursion.py
+++ recursion-and-search/recursion.py
@@ -14,7 +14,11 @@ def factorial(n):
 
 def factorial_iterative(n):
     # TODO: implement the factorial function iteratively here
-    pass
+    product = 1
+    for i in range(1, n + 1):
+        product = product * i
+    return product
+print(factorial)
     # once implemented, change factorial (above) to call factorial_iterative
     # to verify that your iterative implementation passes all tests
 
