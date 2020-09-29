4efbdad8ceaea897ccac962be38fb20bf6fa150a
teknofage <teknofage@gmail.com>
2020-09-28 23:01:47 -0700

    updated LLQ

---
+++ stacks-and-queues/stack-and-queue-NEW/linkedList/linked_list_queue.py
@@ -0,0 +1,15 @@
+from linked_list import LinkedList
+
+class LinkedListQueue:
+
+  def __init__(self):
+    self.ll_queue = LinkedList()
+
+  def enqueue(self, item):
+    self.ll_queue.append(item)
+
+  def dequeue(self):
+    self.ll_queue.delete_from_head()
+
+  def front(self):
+    return self.ll_queue.head.data
